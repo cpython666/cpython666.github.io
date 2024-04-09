3c3cf840ad9e608d62d01356d54f4922292cd460
CodeFly <2942581284@qq.com>
2024-04-09 13:22:39 +0800

    simpleui教程

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -38,7 +38,8 @@ let nav_tmp=[
         items:[
           { text: 'Python设计模式', link: '/Python/系列文章/Python设计模式' },
           { text: 'PyQt6实战开发之旅', link: '/Python/系列文章/PyQt6实战开发之旅' },
-
+          { text: '探索SimpleUI：让Django Admin焕发现代光彩', link: '/Python/系列文章/探索SimpleUI：让Django Admin焕发现代光彩' },
+          
         ]
       },
     ]
